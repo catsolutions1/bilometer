ea4d5b7a43cfd23488741db14b97d4a038775e15
catsolutions1 <143562269+catsolutions1@users.noreply.github.com>
2024-08-31 19:57:17 -0700

    keyboard input player controller

---
+++ src/input/InputStateManager.ts
@@ -0,0 +1,12 @@
+import {KeyboardManager} from "./KeyboardManager.js";
+import {InputState} from "./types.js";
+
+export class InputStateManager {
+  static getInputs = (): InputState => {
+    const keyboardState = KeyboardManager.keyboardState;
+
+    return {
+        keyboard: keyboardState
+    };
+  }
+}
